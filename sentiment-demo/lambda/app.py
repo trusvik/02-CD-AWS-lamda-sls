bbfbbe8ab55745ca3e2d9948f01aac0cd1b98611
trusvik <ec2-user@ip-172-31-40-87.eu-west-1.compute.internal>
2024-09-20 09:15:20 +0000

    made some changes

--- sentiment-demo/lambda/app.py
+++ sentiment-demo/lambda/app.py
@@ -4,10 +4,12 @@ import boto3
 
 def handler(event, context):
 
+    var string = "Hei, dette er en sykt bra test!"
     client = boto3.client('comprehend')
     body = event["body"]
     sentiment = client.detect_sentiment(LanguageCode = "en", Text = body)
     return {
+            console.log(string)
             "statusCode": 200,
             "headers": {
                 "Content-Type": "application/json"
